b4218ac7ce1b44ddb408eac02ebcb0b5b1d0595a
Sand RAZERMERA <sandrazermera@gmail.com>
2025-11-04 15:40:08 +0000

    add purchase_cost & margin

---
+++ models/Intermediate/int_sales_margin.sql
@@ -0,0 +1,9 @@
+SELECT
+    a.quantity,
+    a.revenue,
+    b.purchase_price,
+    quantity*purchase_price AS purchase_cost,
+    revenue - quantity*purchase_price as margin
+FROM {{ ref('stg_raw__sales') }} AS a
+JOIN {{ ref('stg_raw__product') }} AS b
+    ON a.products_id = b.products_id
